2c51f8ccd89b3848ad8024eb027015c84e55756f
Degi100 <r.degering@mail.de>
2021-06-17 00:39:12 +0200

    first notes

---
+++ simpleCalcArroFunction.js
@@ -0,0 +1,69 @@
+function printMessage(){
+	console.log('Hier wird gerechnet + - * /');
+}
+printMessage();
+// Addition 
+let sum = (addA, addB) => addA + addB;
+console.log(sum(2, 3));
+
+let sum2 = (add1, add2) => add1 + add2;
+console.log(sum2(5, 5));
+
+let sum3 = (add3, add4) => add3 + add4;
+console.log(sum3(20,20));
+
+//Multiplikation
+let multiply = (a, b) => a * b;
+console.log(multiply(4,4));
+
+let multiply2 = (multi1, multi2) => multi1 * multi2;
+console.log(multiply(220,10));
+
+//Division
+let quotient = (div1, div2) => div1 / div2;
+console.log(quotient(100, 2));
+
+let quotient2 = (div3, div4) => div3 / div4;
+console.log(quotient2(100, 1000));
+
+let quotient3 = (div3, div4) => div3 / div4;
+console.log(quotient3(256, 64));
+
+//Subtrahtion
+let diff = (sub1, sub2) => sub1 - sub2;
+console.log(diff(500, 250));
+
+let diff2 = (sub3, sub4) => sub3 - sub4;
+console.log(diff2(2021, 1986));
+
+// *****************************************************************
+
+const addiere = numberAdd => numberAdd + 2;
+console.log( addiere (2));
+
+const subtra = numberSub => numberSub - 10;
+console.log(subtra(100));
+
+const multipli = numberMultipli => numberMultipli * 64;
+console.log(multipli(64));
+
+const division = numberDiv => numberDiv / 9;
+console.log(division(999));
+
+// ************************************************************
+
+const message = () => console.log('... loading, es wird weiter gerechnet');
+message();
+
+const message2 = () => console.log('... 33% loading... faild...');
+message2();
+
+const message3 = () => console.log('... String üben');
+message3();
+
+const message4 = () => console.log( '... done');
+message4();
+// **********************************************************
+
+
+
