141d41ca5c3baf37702db6968fa21293e46f6a04
Degi100 <r.degering@mail.de>
2021-06-28 21:57:46 +0200

    add Stuff

---
+++ loops.js
@@ -0,0 +1,6 @@
+let counterA;
+for (let counterA = 0; counterA < 5; counterA += 1) {
+	console.log(counterA);
+}
+
+
---
+++ methods.js
@@ -0,0 +1,98 @@
+console.log(
+  "********************************************************************************"
+);
+
+// .join() 
+// https://www.w3schools.com/jsref/jsref_join.asp
+
+console.log("");
+let methodsArr = [
+  "concat", // done
+  "constructor",
+  "copyWithin", // done
+  "entries", //done
+  "every",
+  "fill", //done
+  "filter",
+  "find",
+  "findIndex",
+  "forEach",
+  "from",
+  "includes",
+  "indexOf",
+  "isArray",
+  "join",
+  "keys",
+  "length",
+  "lastIndexOf",
+  "map",
+  "pop",
+  "prototype",
+  "push",
+  "reduce",
+  "reduceRight",
+  "reserve",
+  "shift",
+  "slice",
+  "some",
+  "sort",
+  "splice",
+  "toString",
+  "unshift",
+  "valueOf",
+];
+joinMethods = methodsArr.join("(), ");
+
+console.log("\x1b[33m" + "METHODS.JOIN: " + joinMethods);
+console.log(
+  "********************************************************************************"
+);
+
+//.concat() 
+// https://www.w3schools.com/jsref/jsref_concat_array.asp
+
+let concatArr1 = ["Concat-0"];
+let concatArr2 = ["Concat-1", "Concat-2"];
+let concatArr3 = ["Concat-3", "Concat-4", "Concat-4"];
+concatMethods = methodsArr
+  .concat(concatArr1, concatArr2, concatArr3)
+  .join("(), ");
+
+console.log("\x1b[32m" + "METHODS.Concat: " + concatMethods);
+console.log(
+  "********************************************************************************"
+);
+
+//.copyWithin()
+// https://www.w3schools.com/jsref/jsref_copywithin.asp
+
+let copyWithinMethods = methodsArr;
+copyWithinMethods.copyWithin(1, 2);
+
+console.log("\x1b[33m" + "METHODS.copyWithin: " + copyWithinMethods);
+console.log(
+  "********************************************************************************"
+);
+
+//https://www.w3schools.com/jsref/jsref_entries.asp
+const entriesMethods = methodsArr.entries();
+for (let result of entriesMethods) {
+  // console.log(result)
+  console.log("\x1b[36m" + "METHODS.entries: " + result.join(". "));
+}
+console.log(
+  "********************************************************************************"
+);
+
+//https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_fill2
+const fillMethods = methodsArr;
+fillMethods.fill("FILL!!!", 2, 25);
+console.log("\x1b[33m" + "METHODS.FILL: " + fillMethods);
+console.log(
+  "********************************************************************************"
+);
+
+// https://docs.w3cub.com/javascript/global_objects/array/find
+
+const findMethods = methodsArr;
+findMethods.find();
